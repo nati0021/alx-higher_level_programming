504b0ef6cbc6b620f6d735675baa22773783263a
belb2 <nathanbiru21@gmail.com>
2022-12-20 22:54:09 +0300

    python 6

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,5 +4,11 @@
 
 
 class Square:
-    """Does nothing"""
-    pass
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+
+        self.__size = size
