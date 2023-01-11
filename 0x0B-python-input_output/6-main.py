c2c99812535928eb36905bcac4d33e11307d909b
belb2 <nathanbiru21@gmail.com>
2023-01-12 00:47:38 +0300

    all

---
+++ 0x0B-python-input_output/6-main.py
@@ -0,0 +1,6 @@
+[1, 2, 3]
+<class 'list'>
+{'name': 'John', 'info': {'age': 36, 'average': 3.14}, 'id': 12, 'places': ['San Francisco', 'Tokyo'], 'is_active': True}
+<class 'dict'>
+[FileNotFoundError] [Errno 2] No such file or directory: 'my_set_doesnt_exist.json'
+[ValueError] Expecting property name enclosed in double quotes: line 1 column 21 (char 20)
